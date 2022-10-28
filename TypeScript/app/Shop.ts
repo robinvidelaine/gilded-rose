d8609e481eaba195784029c7bce3c3bf9346a286
RobinVidelaine <robinvidelaine@gmail.com>
2022-10-28 11:33:54 +0200

    v0.2

---
+++ TypeScript/app/Shop.ts
@@ -0,0 +1,23 @@
+
+import { 
+    updateQualityForAgedBrie, 
+    updateQualityForConcert, 
+    updateQualityForSulfuras, 
+    updateQualityForConjured, 
+    updateQualityForNormalItem} from './updateQuality'
+
+export class GildedRose {
+    items: Array<Item>;
+
+    constructor(items = [] as Array<Item>) {
+        this.items = items;
+    }
+
+    update() : void {
+        this.items.forEach(function(item){
+            item.updateSellIn();
+            item.updateQuality();
+        }
+        )
+    }
+}
